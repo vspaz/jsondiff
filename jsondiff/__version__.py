6d2805d0566d49687f6c845bb1d720456a8bb217
vspaz <vladimir.edelman@gmail.com>
2021-11-16 20:12:34 +0100

    add '__version__' file

---
+++ jsondiff/__version__.py
@@ -0,0 +1,11 @@
+__title__ = 'jsondiff'
+__description__ = (
+    "'jsondiff' is a utility to calculates json diff among 2 files applying "
+    "different number precision, leaving out optional fields or "
+    "picking the required fields, only; it's also possible to use some field prefix or regex to pick fields"
+)
+__url__ = 'https://github.com/vspaz/pyclient'
+__email__ = ''
+__author__ = 'vspaz'
+__maintainer__ = 'vspaz'
+__version__ = '0.1.1'
