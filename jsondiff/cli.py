b3bf6092cef8f6d58fe2e36311f266fb99fa108c
vspaz <vladimir.edelman@gmail.com>
2021-11-17 19:16:13 +0100

    add make config optional; add basic validation

--- jsondiff/cli.py
+++ jsondiff/cli.py
@@ -3,9 +3,9 @@ import logging
 import sys
 
 
-class ValidateNonEmpty(argparse.Action):
+class AssertFileNonEmpty(argparse.Action):
     def __init__(self, option_strings, dest, **kwargs):
-        super(ValidateNonEmpty, self).__init__(option_strings, dest, **kwargs)
+        super().__init__(option_strings, dest, **kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         json_string = values.read()
@@ -15,28 +15,39 @@ class ValidateNonEmpty(argparse.Action):
             except ValueError as e:
                 logging.error(e)
                 sys.exit(1)
-        else:
-            setattr(namespace, self.dest, json_string)
+        setattr(namespace, self.dest, json_string)
+
+
+class ValidateConfig(argparse.Action):
+    def __init__(self, option_strings, dest, **kwargs):
+        super().__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        with open(values) as fh:
+            json_string = fh.read()
+        if not json_string:
+            logging.warning('empty config given; ignoring')
+            json_string = '{}'
+        setattr(namespace, self.dest, json_string)
 
 
 def get_args():
     description = 'json comparison utility'
-
     argparser = argparse.ArgumentParser(description)
     argparser.add_argument(
-        '-c', '--config', type=argparse.FileType('r'), dest='config',
-        required=False, default='{}', help='<config.json>',
-        action=ValidateNonEmpty,
+        '-c', '--config', type=str, dest='config',
+        required=False, default='{}', help='<path/to/config.json>',
+        action=ValidateConfig,
     )
 
     argparser.add_argument(
         '-f1', '--file1', type=argparse.FileType('r'),
-        dest='file1', required=True, help='<file1.json>',
-        action=ValidateNonEmpty,
+        dest='file1', required=True, help='<path/to/file1.json>',
+        action=AssertFileNonEmpty,
     )
     argparser.add_argument(
         '-f2', '--file2', type=argparse.FileType('r'),
-        dest='file2', required=True, help='<file2.json>',
-        action=ValidateNonEmpty,
+        dest='file2', required=True, help='<path/to/file2.json>',
+        action=AssertFileNonEmpty,
     )
     return argparser.parse_args()
